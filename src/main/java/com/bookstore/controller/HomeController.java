ead484e2041346e6fc01f25f06e253ff2c8c925b
Kirill Kustov <kirinator3@gmail.com>
2018-08-19 18:08:55 +0300

    1

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -1,12 +1,42 @@
 package com.bookstore.controller;
 
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.Role;
+import com.bookstore.domain.security.UserRole;
+import com.bookstore.service.impl.UserSecurityService;
+import com.bookstore.service.impl.UserService;
+import com.bookstore.utility.SecurityUtility;
+import org.apache.catalina.security.SecurityUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
 
     @RequestMapping("/")
     public String index(){
@@ -20,10 +50,67 @@ public class HomeController {
         return "myAccount";
     }
 
+    @PostMapping("/newUser")
+    public String newUserPost(HttpServletRequest request, @ModelAttribute("email") String userEmail,
+                              @ModelAttribute("username") String username,
+                              Model model) throws Exception{
+        model.addAttribute("classActiveNewAccount", true);
+        model.addAttribute("email", userEmail);
+        model.addAttribute("userName", username);
+
+        if(userService.findByUsername(username)!=null){
+            model.addAttribute("usernameExists",true);
+
+            return "myAccount";
+        }
+
+        if(userService.findByEmail(userEmail)!=null){
+            model.addAttribute("email",true);
+
+            return "myAccount";
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(userEmail);
+
+        String password = SecurityUtility.randomPassword();
+
+        String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
+        user.setPassword(encryptedPassword);
+
+        Role role = new Role();
+        role.setRoleId(1);
+        role.setName("ROLE_USER");
+        Set<UserRole>  userRoles = new HashSet<>();
+        userRoles.add(new UserRole(user,role));
+        userService.createUser(user,userRoles);
+
+
+    }
+
     @RequestMapping("/newUser")
-    public String newUser(Model model){
-        model.addAttribute("classActiveNewAccount",true);
-        return "myAccount";
+    public String newUser(Model model, Locale locale, @RequestParam("token") String token){
+        PasswordResetToken passwordResetToken= userService.getPasswordResetToken(token);
+
+
+        if(passwordResetToken == null){
+            String message = "invalid Token.";
+            model.addAttribute("message",message);
+            return "redirect:/badRequest";
+        }
+
+        User user = passwordResetToken.getUser();
+        String username = user.getUsername();
+
+        UserDetails userDetails = userSecurityService.loadUserByUsername(username);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,userDetails.getPassword(),
+                userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        model.addAttribute("classActiveEdit",true);
+        return "myProfile";
     }
 
     @RequestMapping("/forgetPassword")
